8aeea825269cc98e6890b2630e7ce84d7cd6ff8c
moslik <mosol.07@mail.ru>
2021-06-11 11:32:24 +0400

    migrate su

---
+++ sqlite.py
@@ -0,0 +1,15 @@
+
+import sqlite3
+
+conn = sqlite3.connect('db.sqlite3')
+cursor = conn.cursor()
+
+cursor.execute("""select * from sqlite_master
+            where type = 'table'""")
+tables = cursor.fetchall()
+
+for table in tables:
+  print(table) # информация о таблицах
+  print("-----------------")
+#  print(table[1]) #названия таблиц
+
