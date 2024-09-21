a6673ae483679ff97db85831130a9fd4c95e704d
mosl1k <ko.yash@mail.ru>
2024-09-21 09:59:40 +0400

    node-status

--- app/main/urls.py
+++ app/main/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('create', views.create, name='create'),
     path('geshtalt', views.geshtalt, name='geshtalt'),
-    path('node-status/', views.get_nodes_status, name='node_status'),
+    path('node-status', views.get_nodes_status, name='node_status'),
 ]
